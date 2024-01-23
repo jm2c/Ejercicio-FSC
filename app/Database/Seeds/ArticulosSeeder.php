ff2082519155a081635c13f6e526b3d796b547c2
Juan Manuel Careaga <jm2002c@gmail.com>
2024-01-23 12:12:18 -0600

    Seeder de prueba para generar articulos

---
+++ app/Database/Seeds/ArticulosSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use Faker\Factory;
+
+class ArticulosSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Factory::create();
+        $data = [];
+
+        for($i = 0; $i < 20; $i++)
+        {
+            $min = $faker->randomNumber() % 6; // [0 - 5]
+            $max = $min + $faker->randomNumber() % 4 + 1; // $min + [1 - 4]
+
+            $data[] = [
+                'titulo' => $faker->sentence(),
+                'palabras_clave' => $faker->words(4, true),
+                'edad_minima' => $min,
+                'edad_maxima' => $max,
+                'imagen_portada' => $faker->imageUrl(800, 640),
+                'imagen_previa' => $faker->imageUrl(30,30),
+                'sintesis' => $faker->text(200),
+                'contenido' => $faker->text(800)
+            ];
+        }
+
+        $db = \Config\Database::connect();
+        $builder = $db->table('articulos');
+        $builder->insertBatch($data);
+    }
+}
