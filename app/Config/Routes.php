6fcf418d3dae578f7422919ae9513df89a842ad6
Juan Manuel Careaga <jm2002c@gmail.com>
2024-01-23 18:01:36 -0600

    Punto de acceso para los artículos de portada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Articulos::index');
+$routes->get('/listaArticulosPortada', 'Articulos::listaArticulosPortada');
--- app/Controllers/Articulos.php
+++ app/Controllers/Articulos.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ArticuloModel;
 
 class Articulos extends BaseController
 {
@@ -12,5 +13,15 @@ class Articulos extends BaseController
         return view('layout/header')
              . view('inicio')
              . view('layout/footer');
+    }
+
+    public function listaArticulosPortada()
+    {
+        $articulo = new ArticuloModel();
+
+        $articulosPortada = $articulo->select(['id', 'titulo', 'imagen_previa', 'sintesis'])
+                          ->orderBy('fecha_de_creacion', 'desc')->limit(6)->find();
+
+        return json_encode($articulosPortada);
     }
 }
