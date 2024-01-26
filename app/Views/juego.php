f8a469432ad81ea9bda3ee4124d26ab45b282c69
Juan Manuel Careaga <jm2002c@gmail.com>
2024-01-26 01:08:17 -0600

    Objetos arrastrables

--- app/Views/juego.php
+++ app/Views/juego.php
@@ -40,11 +40,72 @@
             "/assets/triangleLeft.png",
             "/assets/triangleRight.png"
             ]
+        let left
+
+        class Triangle {
+            constructor(x,y) {
+                this.x = x || WIDTH / 2
+                this.y = y || HEIGHT / 2
+                this.touching = false
+            }
+
+            isTouching(x,y) {
+                const err    = 10 // ratio of error in pixels
+                const top    = this.y - err
+                const left   = this.x - err
+                const bottom = this.y + SCALE + err
+                const right  = this.x + SCALE + err
+
+                return (x > left && x < right) && (y < bottom && y > top)
+            }
+
+            move(x, y) {
+                this.x = x - SCALE / 2
+                this.y = y - SCALE / 2
+            }
+        }
+
+        class LeftTriangle extends Triangle {
+            constructor() {
+                super()
+                this.draw()
+            }
+
+            draw() {
+                ctx.drawImage(images[2], this.x, this.y, SCALE, SCALE)
+            }
+        }
+
+        canvas.onpointerdown = evt => {
+            evt.preventDefault()
+            let x = evt.clientX
+            let y = evt.clientY
+            left.touching = left.isTouching(x, y)
+        }
+
+        canvas.onpointermove = evt => {
+            evt.preventDefault()
+            x = evt.clientX
+            y = evt.clientY
+            if(left.touching)
+                left.move(x,y)
+        }
+
+        canvas.onpointerup = evt => {
+            evt.preventDefault()
+            left.touching = false
+        }
+
+        window.addEventListener("resize", function(e) {
+            init();
+        });
 
         async function init() {
             canvas.width = WIDTH
             canvas.height = HEIGHT
             images = await preloadImages(imgUrls)
+
+            left = new LeftTriangle()
 
             draw()
             update()
@@ -55,6 +116,8 @@
             ctx.drawImage(images[1], LEFT,  BOTTOM, SCALE, SCALE)
             ctx.drawImage(images[2], RIGHT, TOP, SCALE, SCALE)
             ctx.drawImage(images[3], RIGHT, BOTTOM, SCALE, SCALE)
+
+            left.draw()
         }
 
         function update() {
