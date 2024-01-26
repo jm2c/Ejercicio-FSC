2cdac932c06a3ef7a1724b6e2cc9fc5147ab65ba
Juan Manuel Careaga <jm2002c@gmail.com>
2024-01-26 09:48:48 -0600

    Objetos arrastrables 2

--- app/Views/juego.php
+++ app/Views/juego.php
@@ -17,6 +17,7 @@
         #juego-canvas {
             margin: 0 auto;
             background-color: #222;
+            cursor: pointer
         }
     </style>
     <title>Juego</title>
@@ -28,25 +29,34 @@
     <script>
         const WIDTH  = window.innerWidth
         const HEIGHT = window.innerHeight
-        const SCALE = WIDTH/20
+        const SCALE = WIDTH/15
         const [LEFT, TOP, BOTTOM, RIGHT] = [0, 0, HEIGHT-SCALE, WIDTH-SCALE]
         const canvas = document.getElementById('juego-canvas')
         const ctx    = canvas.getContext("2d")
-        let images = []
+        let imagesArray = []
+        let images = {}
+        let goalsImagesArray = []
+
+        let isDragging = false
         const imgUrls = [
             /* https://reffpixels.itch.io/genericicons */
             "/assets/triangleUp.png",
             "/assets/triangleDown.png",
             "/assets/triangleLeft.png",
-            "/assets/triangleRight.png"
+            "/assets/triangleRight.png",
+            "/assets/goalUp.png",
+            "/assets/goalDown.png",
+            "/assets/goalLeft.png",
+            "/assets/goalRight.png"
             ]
-        let left
+        let figures = []
 
         class Triangle {
             constructor(x,y) {
                 this.x = x || WIDTH / 2
                 this.y = y || HEIGHT / 2
                 this.touching = false
+                this.image = undefined
             }
 
             isTouching(x,y) {
@@ -63,16 +73,45 @@
                 this.x = x - SCALE / 2
                 this.y = y - SCALE / 2
             }
+
+            draw() {
+                ctx.drawImage(this.image, this.x, this.y, SCALE, SCALE)
+            }
+        }
+
+        class UpTriangle extends Triangle {
+            constructor(x,y) {
+                super(x,y)
+                this.imageIndex = 0
+                this.image = imagesArray[this.imageIndex]
+                this.type = 'UP'
+            }
         }
 
         class LeftTriangle extends Triangle {
-            constructor() {
-                super()
-                this.draw()
+            constructor(x,y) {
+                super(x,y)
+                this.imageIndex = 2
+                this.image = imagesArray[this.imageIndex]
+                this.type = 'LEFT'
             }
+        }
 
-            draw() {
-                ctx.drawImage(images[2], this.x, this.y, SCALE, SCALE)
+        class DownTriangle extends Triangle {
+            constructor(x,y) {
+                super(x,y)
+                this.imageIndex = 1
+                this.image = imagesArray[this.imageIndex]
+                this.type = 'DOWN'
+            }
+        }
+
+        class RightTriangle extends Triangle {
+            constructor(x,y) {
+                super(x,y)
+                this.imageIndex = 3
+                this.image = imagesArray[this.imageIndex]
+                this.type = 'RIGHT'
             }
         }
 
@@ -80,44 +119,90 @@
             evt.preventDefault()
             let x = evt.clientX
             let y = evt.clientY
-            left.touching = left.isTouching(x, y)
+            figures.forEach(fig => {
+                let t = !isDragging && fig.isTouching(x, y)
+                if(t) {
+                    fig.touching = isDragging = true
+                    fig.image = imagesArray[fig.imageIndex + 4]
+                }
+            })
         }
 
         canvas.onpointermove = evt => {
             evt.preventDefault()
             x = evt.clientX
             y = evt.clientY
-            if(left.touching)
-                left.move(x,y)
+            figures.forEach(fig => {
+                if(fig.touching) fig.move(x,y)
+            })
         }
 
         canvas.onpointerup = evt => {
             evt.preventDefault()
-            left.touching = false
+            figures.forEach(fig => {
+                fig.touching = false
+                fig.image = imagesArray[fig.imageIndex]
+            })
+            isDragging = false
         }
-
-        window.addEventListener("resize", function(e) {
-            init();
-        });
 
         async function init() {
             canvas.width = WIDTH
             canvas.height = HEIGHT
-            images = await preloadImages(imgUrls)
+            imagesArray = await preloadImages(imgUrls)
+            images = {
+                UP     : imagesArray[0],
+                DOWN   : imagesArray[1],
+                LEFT   : imagesArray[2],
+                RIGHT  : imagesArray[3],
+                GUP    : imagesArray[4],
+                GDOWN  : imagesArray[5],
+                GLEFT  : imagesArray[6],
+                GRIGHT : imagesArray[7]
+            }
+            goalsImagesArray = shuffleArray([images.GUP, images.GDOWN, images.GLEFT, images.GRIGHT])
 
-            left = new LeftTriangle()
+            for(let i = 0; i < 2; i++){
+                const fig = new LeftTriangle(i * SCALE + 40, i * SCALE + 40)
+                figures.push(fig)
+            }
 
             draw()
             update()
         }
 
         function draw() {
-            ctx.drawImage(images[0], LEFT,  TOP, SCALE, SCALE)
-            ctx.drawImage(images[1], LEFT,  BOTTOM, SCALE, SCALE)
-            ctx.drawImage(images[2], RIGHT, TOP, SCALE, SCALE)
-            ctx.drawImage(images[3], RIGHT, BOTTOM, SCALE, SCALE)
+            // Draw the arcs
+            ctx.strokeStyle = "#55FF55"
+            ctx.fillStyle = "#006600"
+            ctx.beginPath()
+            ctx.arc(0,0, 2.5*SCALE, 0, 2*Math.PI)
+            ctx.stroke()
+            ctx.fill()
+            ctx.beginPath()
+            ctx.arc(WIDTH,0, 2.5*SCALE, 0, 2*Math.PI)
+            ctx.stroke()
+            ctx.fill()
+            ctx.beginPath()
+            ctx.arc(WIDTH,HEIGHT, 2.5*SCALE, 0, 2*Math.PI)
+            ctx.stroke()
+            ctx.fill()
+            ctx.beginPath()
+            ctx.arc(0,HEIGHT, 2.5*SCALE, 0, 2*Math.PI)
+            ctx.stroke()
+            ctx.fill()
 
-            left.draw()
+            // Draw goal figures
+            ctx.drawImage(goalsImagesArray[0], LEFT,  TOP, SCALE, SCALE)
+            ctx.drawImage(goalsImagesArray[1], LEFT,  BOTTOM, SCALE, SCALE)
+            ctx.drawImage(goalsImagesArray[2], RIGHT, TOP, SCALE, SCALE)
+            ctx.drawImage(goalsImagesArray[3], RIGHT, BOTTOM, SCALE, SCALE)
+
+
+
+            figures.forEach(fig => {
+                fig.draw()
+            })
         }
 
         function update() {
